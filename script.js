780ee7a8646e9844a06edef4f18156c38d37b72c
cvncerboy <74409676+cvncerboy@users.noreply.github.com>
2021-06-22 15:52:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const scroll = document.querySelector('.navTop');
-
+const more = document.querySelector('.moree');
+const drop = document.querySelector('.dropdown');
 
 scroll.addEventListener('click', () => {
 	document.documentElement.scrollTop = '0';
+	return;
+});
+
+drop.addEventListener('mouseover', () => {
+	more.style.display = 'block';
+	return;
+});
+
+drop.addEventListener('mouseout', () => {
+	more.style.display = 'none';
+	return;
 });
